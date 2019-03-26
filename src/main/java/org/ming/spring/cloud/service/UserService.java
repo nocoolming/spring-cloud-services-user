29f99ae301a84ebdcc5e3d4bc5e542a7b42bf492
finch <21374618@qq.com>
2019-03-26 09:55:39 +0800

    增加了个post接口

--- src/main/java/org/ming/spring/cloud/service/UserService.java
+++ src/main/java/org/ming/spring/cloud/service/UserService.java
@@ -1,9 +1,7 @@
 package org.ming.spring.cloud.service;
 
 import org.ming.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +9,7 @@ import java.util.List;
 @RestController
 public class UserService {
     @RequestMapping("all")
-    List<User> all(){
+    List<User> all() {
         List<User> users = new ArrayList<>();
 
         users.add(new User("liming", "伯通 "));
@@ -28,5 +26,10 @@ public class UserService {
         users.add(new User("huangrong", "黄容"));
 
         return users;
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    User create(@RequestBody User user) {
+        return user;
     }
 }
