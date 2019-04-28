84965b0f3bfa78e0ff46efebc0ca5a203258a0e6
finch <21374618@qq.com>
2019-04-28 18:15:20 +0800

    integration agent for monitor skywalking

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,17 +7,6 @@ pipeline {
                 sh './build.sh'
             }
         }
-        stage('Push') {
-            steps {
-                sh 'chmod +x push.sh'
-                sh './push.sh'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                sh 'chmod +x deploy.sh'
-                sh './deploy.sh'
-            }
-        }
+
     }
 }
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # docker image version
-VERSION=2.2
+VERSION=2.3
 
 echo maven packaging
 chmod +x mvnw
@@ -12,3 +12,30 @@ docker rmi nocoolming/service-user:$VERSION
 
 echo build new docker
 docker build -t nocoolming/service-user:$VERSION .
+
+
+#push
+echo login to aliyun docker registry
+cat password.txt | docker login --username=nocoolming@aliyun.com registry.cn-shanghai.aliyuncs.com --password-stdin
+
+echo remove docker image on local
+docker rmi registry.cn-shanghai.aliyuncs.com/nocoolming/service-user:$VERSION
+
+echo tag docker image
+docker tag nocoolming/service-user:$VERSION registry.cn-shanghai.aliyuncs.com/nocoolming/service-user:$VERSION
+
+echo pushing to aliyun docker registry
+docker push registry.cn-shanghai.aliyuncs.com/nocoolming/service-user:$VERSION
+
+
+# Deploy
+# project path
+PROJECT_PATH=~/projects/spring-cloud/service-user/
+
+
+echo uploading latest version service-user.yaml
+scp service-user.yaml root@192.168.1.171:$PROJECT_PATH/service-user.yaml
+
+echo upgrade service on kubernetes
+ssh root@192.168.1.171 $PROJECT_PATH/upgrade.sh $VERSION
+
